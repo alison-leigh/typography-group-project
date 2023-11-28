6520638cc51046234a83daf7d411ea51d8cc7f2a
Alison Leigh <65463285+alison-leigh@users.noreply.github.com>
2023-11-27 20:33:47 -0500

    QUIZ WORKS

---
+++ js/chart.js
@@ -0,0 +1,35 @@
+function displayResultsChart() {
+    var ctx = document.getElementById('resultsChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Times New Roman', 'Comic Sans', 'Lobster', 'Ubuntu', 'Homemade Apple'],
+            datasets: [{
+               // label: 'Personality Score',
+                data: userStats,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(85, 182, 100, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(85, 182, 100, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
--- js/quiz.js
+++ js/quiz.js
@@ -7,60 +7,58 @@ var quizActive = true;
 // and modify the point assignment by adding +1 for each new personality added.
 
 var userStats =	[
-    0,	// choleric: ambitious, passionate, efficient, aggressive, impatient, argumentative
-    0, 	// melancholic: thoughtful, organized, creative, obsessive, perfectionist, moody
-    0, 	// phlegmatic: relaxed, kind, observant, shy, stubborn, lazy
-    0, 	// sanguine: sociable, charismatic, optimistic, impulsive, shameless, exaggerator
+    0,	// Times New Roman: Classic, stuffy, traditional
+    0, 	// Comic Sans: Goofy, clownish
+    0, 	// Lobster: Gregarious, popular, jock-ish
+    0, 	// Ubuntu: weeaboo
+    0, // Homemade Apple: country chic
 ];
 
 var tempStats = userStats; // temporarily holds the user stats
 
 // stores the text fpr each question, separated by commas
 var questionText =	[
-    "1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.", // Question #1 (answers: lorem1, ipsum, dolor, sit, amet, consectetur)
-    "2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.", // Question #2 (answers: lorem2, ipsum, dolor, sit, amet, consectetur)
-    "3. Lorem ipsum dolor sit amet, consectetur adipisicing elit.", // Question #3 (answers: lorem3, ipsum, dolor, sit, amet, consectetur)
+    "1. Which streaming service is your favorite?", // Question #1 (answers: lorem1, ipsum, dolor, sit, amet, consectetur)
 ];
 
 // the arrays contain answers for each question, separated by commas.
 // each question should have the same amount of possible answers for math reasons (scroll down to var answerValues below)
 
 var answerText =	[
-    /* Q1 Options */ [ "Lorem1", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur"],
-    /* Q2 Options */ [ "Lorem2", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur"],
-    /* Q3 Options */ [ "Lorem3", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur"],
+    /* Q1 Options */ [ "Peacock" /*apple*/, "Crunchyroll" /*ubuntu*/, "ESPN" /*lobster*/ , "Cable" /*TNR*/, "Prime" /*comic*/],
+    // /* Q2 Options */ [ "Lorem2", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur"],
+    // /* Q3 Options */ [ "Lorem3", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur"],
 ]
 
 
 // assign a value from 0-3 points for each column representing the four personality types
 // use each number only once per answer
-// column 1: choleric, column 2: melancholic, column 3: phlegmatic, column 4: sanguine
+//1 Times New Roman, 2 Comic Sans, 3 Lobster, 4 Ubuntu, 5 Homemade Apple
 // you can add as many questions as you want, but the array length should remain the same to correspond to the personalities/fonts
 
 var answerValues =	[		//question 1 answer values
-    /* Q#1 */    [ [3,2,1,0], // "Answer 1"
-        [0,3,1,2], // "Answer 2"
-        [3,0,2,1], // "Answer 3"
-        [3,0,1,2], // "Answer 4"
-        [0,2,1,3], // "Answer 5"
-        [1,0,3,2]  // "Answer 6"
-    ],
-
-    /* Q#2 */    [ [3,2,1,0], // "Answer 1"
-        [0,3,1,2], // "Answer 2"
-        [3,0,2,1], // "Answer 3"
-        [3,0,1,2], // "Answer 4"
-        [0,2,1,3], // "Answer 5"
-        [1,0,3,2]  // "Answer 6"
-    ],
-
-    /* Q#3 */    [ [3,2,1,0], // "Answer 1"
-        [0,3,1,2], // "Answer 2"
-        [3,0,2,1], // "Answer 3"
-        [3,0,1,2], // "Answer 4"
-        [0,2,1,3], // "Answer 5"
-        [1,0,3,2]  // "Answer 6"
-    ]
+    /* Q#1 */    [ [0,0,0,0,4], // "Peacock"
+        [0,0,0,4,0], // "Crunchyroll"
+        [0,0,4,0,0], // "ESPN"
+        [4,0,0,0,0], // "Cable"
+        [0,4,0,0,0] // "Prime"
+     ]//,
+    //
+    // /* Q#2 */    [ [3,2,1,0], // "Answer 1"
+    //     [0,3,1,2], // "Answer 2"
+    //     [3,0,2,1], // "Answer 3"
+    //     [3,0,1,2], // "Answer 4"
+    //     [0,2,1,3], // "Answer 5"
+    //     [1,0,3,2]  // "Answer 6"
+    // ],
+    //
+    // /* Q#3 */    [ [3,2,1,0], // "Answer 1"
+    //     [0,3,1,2], // "Answer 2"
+    //     [3,0,2,1], // "Answer 3"
+    //     [3,0,1,2], // "Answer 4"
+    //     [0,2,1,3], // "Answer 5"
+    //     [1,0,3,2]  // "Answer 6"
+    // ]
 ]
 
 // DOM references
@@ -163,34 +161,41 @@ function setCustomPage() {
 // these are the results! the js modifies the css of the page so each result can have its unique page.
 
 function displayCustomPage(personality) {
+    displayResultsChart();
     switch (personality) {
 
-        case 0:	// font 1
+        case 0:	// Times New Roman
             results.style.display = "inline-block";
-            results.classList.add("font1");
+            results.classList.add("tnr");
             body.background = "none";
-            printResult.innerText = "Font 1";
+            printResult.innerText = "Times New Roman";
             break;
 
-        case 1:		// font 2
+        case 1:		// Comic Sans
             results.style.display = "inline-block";
             results.classList.add("font2");
             body.background = "none";
-            printResult.innerText = "Font 2";
+            printResult.innerText = "Comic Sans";
             break;
 
-        case 2: // font 3
+        case 2: // Lobster
             results.style.display = "inline-block";
             results.classList.add("font3");
             body.background = "none";
-            printResult.innerText = "Font 3";
+            printResult.innerText = "Lobster";
             break;
 
-        case 3:		// font 4
+        case 3:		// Ubuntu
             results.style.display = "inline-block";
             results.classList.add("font4");
             body.background = "none";
-            printResult.innerText = "Font 4";
+            printResult.innerText = "Ubuntu";
+            break;
+        case 4:		// Homemade Apple
+            results.style.display = "inline-block";
+            results.classList.add("font4");
+            body.background = "none";
+            printResult.innerText = "Homemade Apple";
             break;
 
 
